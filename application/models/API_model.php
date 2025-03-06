8a96aebe4cd117018a77c7c42a025c678fefa0b7
Hansel mejarito <mejarito.hansel@gmail.com>
2025-03-07 03:26:03 +0800

    update CORS

--- application/models/API_model.php
+++ application/models/API_model.php
@@ -5,10 +5,18 @@
 
 
 		public function __construct() {
-	        header('Access-Control-Allow-Origin: *');
-	    	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-	    	header("Content-type: application/json");
-	    	parent::__construct();
+		    header('Access-Control-Allow-Origin: *');
+		    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+		    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+		    header("Content-Type: application/json");
+
+		    // Handle OPTIONS request (Preflight)
+		    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+		        http_response_code(200);
+		        exit();
+		    }
+
+		    parent::__construct();
 		}
 
 		function backup(){
